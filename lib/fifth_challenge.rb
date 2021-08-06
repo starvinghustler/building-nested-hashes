5afe444460ab6ffdb589073ad9e5f01cbb286bd5
ihollander <ianholla@gmail.com>
2021-08-06 09:47:40 -0400

    Fix fifth_challenge to match readme

--- lib/fifth_challenge.rb
+++ lib/fifth_challenge.rb
@@ -9,7 +9,7 @@ def fifth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Steven", age: "30", attitude: "confused"}
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -17,8 +17,8 @@ def fifth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
-          {name: "Nurse", age: "44", attitude: "worried"}
+         {name: "Steven", age: "30", attitude: "confused"},
+         {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
   }
